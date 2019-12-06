28bbd41e94e8d671e98b7c7076f002a273114025
Hani Herbert Ho <hani.ho@pointwest.com.ph>
2019-12-06 15:12:38 +0800

    Added CardDataService.getCard(). Populated CardDataService with card data.

--- src/app/card/card-data.service.ts
+++ src/app/card/card-data.service.ts
@@ -8,5 +8,31 @@ import { ICard } from './card';
 export class CardDataService {
   cardList: ICard[];
 
-  constructor() { }
+  getCard(id: number): ICard {
+    return this.cardList[id];
+  }
+
+  constructor() { 
+    this.initCardList();
+  }
+
+  initCardList() {
+    this.cardList = [
+      {
+        id: 1,
+        title: "Drink Water!",
+        description: "Drink 8 glasses of water! Stay hydrated, king!"
+      },
+      {
+        id: 2,
+        title: "Read a book",
+        description: "It can be any book, a technical book, or just for enjoyment."
+      },
+      {
+        id: 3,
+        title: "Finish Gym Module",
+        description: "Make sure it includes proper routing."
+      }
+    ]
+  }
 }
